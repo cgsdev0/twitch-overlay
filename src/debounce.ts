0ed516752ccb9d91f424587dd28bf68107ccfb77
Sarah Schulte <git@sarah.engineer>
2023-07-14 03:59:31 -0700

    delete this

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,13 +1,3 @@
-export function debounce(func: any, timeout: number) {
-  let timer: any;
-  return (...args: any[]) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
-}
-
 export function throttle(cb: any, delay: number) {
   let wait = false;
 
